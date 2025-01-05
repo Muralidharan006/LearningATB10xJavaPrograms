f5591ecc6c0c2ae21fb677963170249c3d8454b8
Muralidharan006 <muraliraj2k@gmail.com>
2025-01-05 12:03:12 +0530

    first commit

---
+++ src/ex_001_Java_Basic/Lab006_GitCommitAndPush.java
@@ -0,0 +1,9 @@
+package ex_001_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab006_GitCommitAndPush {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
