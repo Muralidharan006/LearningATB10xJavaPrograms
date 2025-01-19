413863461a18922bc407b926d4b3a3e36a66682b
Muralidharan006 <muraliraj2k@gmail.com>
2025-01-19 17:45:38 +0530

    new commit19th jan task

--- src/ex_06_Ternary_Operator/Lab45_Nester_TO.java
+++ src/ex_06_Ternary_Operator/Lab45_Nester_TO.java
@@ -3,7 +3,12 @@ package ex_06_Ternary_Operator;
 public class Lab45_Nester_TO {
     //Nested Ternary
 
-    int number = 15;
-    //String result = (number > 10) ? (number > 20 ? ">20":"B 10 and 20");
+
+    public static void main(String[] args) {
+
+        int number = 15;
+        String result = (number > 10) ? (number > 20 ? ">20":"B 10 and 20"):"<10";
+        System.out.println(result);
+    }
 
 }
---
+++ src/ex_06_Ternary_Operator/TO_Task_19thJAN.java
@@ -0,0 +1,27 @@
+package ex_06_Ternary_Operator;
+
+public class TO_Task_19thJAN {
+    //*Write a program that calculates and displays the letter grade for a given numerical score (e.g., A, B, C, D, or F) based on the following grading scale:
+    //
+    //A: 90-100
+    //
+    //B: 80-89
+    //
+    //C: 70-79
+    //
+    //D: 60-69
+    //
+    //F: 0-59
+
+    //Input = int, output = char
+    //Logic building
+//String result = (number > 10) ? (number > 20 ? ">20":"B 10 and 20"):"<10";
+
+    public static void main(String[] args) {
+        int mark = 95;
+        //String Result = (mark<=59) ? "F":(mark<=69)?"D":(mark<=79)?"C":(mark<=89)?"B":(mark<=100)?"A";
+        String Result = (mark<=59)? "F": ((mark<=69)?"D":(mark<=79)?"C":(mark<=89)?"B":"A");
+        System.out.println(Result);
+    }
+
+}
